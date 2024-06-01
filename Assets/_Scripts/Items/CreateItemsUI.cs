d4711623a7ce6f88aaa3b264c208405d11a15147
TrapTap277 <145903439+TrapTap277@users.noreply.github.com>
2024-06-01 12:48:04 +0300

    Added 2d Skill Icons

--- Assets/_Scripts/Items/CreateItemsUI.cs
+++ Assets/_Scripts/Items/CreateItemsUI.cs
@@ -6,7 +6,14 @@ namespace _Scripts.Items
     {
         [SerializeField] private GameObject _itemParent;
         [SerializeField] private CurrentItem _currentItem;
+
+        public HealGemItem HealGemItem;
         
+        private void Start()
+        {
+            Create(HealGemItem);
+        }
+
         public void Create(BaseItem item)
         {
             CurrentItem newItem = Instantiate(_currentItem, _itemParent.transform);
