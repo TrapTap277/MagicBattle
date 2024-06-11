3e1880a12b88086cb7a747a88a37508c9cb75ffc
TrapTap277 <145903439+TrapTap277@users.noreply.github.com>
2024-06-11 12:32:02 +0300

    Added Start Scene
    - Start Scene
    - Scene change scripts

---
+++ Assets/_Scripts/SwitchScene/ChangeScene.cs
@@ -0,0 +1,48 @@
+﻿using _Scripts.Move;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Scripts.SwitchScene
+{
+    public class ChangeScene : MonoBehaviour
+    {
+        private CanvasGroup _darkness;
+        private Animator _darknessAnimator;
+        private const float TIME_TO_DARKNESS = 3f;
+
+        private void Start()
+        {
+            _darkness = GameObject.FindGameObjectWithTag("Darkness").GetComponent<CanvasGroup>();
+            _darknessAnimator = _darkness.GetComponent<Animator>();
+        }
+
+        public void ChangeCurrentScene(int index)
+        {
+            if (_darknessAnimator != null)
+                _darknessAnimator.enabled = false;
+
+            Sequence darkness = DOTween.Sequence();
+            darkness.Append(_darkness.DOFade(1, TIME_TO_DARKNESS).SetEase(Ease.Linear)).OnComplete(() => ChangeCurrentSceneAfterDarkness(index));
+        }
+
+        private void ChangeCurrentSceneAfterDarkness(int index)
+        {
+            SceneManager.LoadScene(index);
+        }
+
+        private void OnEnable()
+        {
+            PlayerLost.OnChangedScene += ChangeCurrentScene;
+            GoToPortal.OnChangedScene += ChangeCurrentScene;
+            VignetteLerp.OnChengedScene += ChangeCurrentScene;
+        }
+
+        private void OnDisable()
+        {
+            PlayerLost.OnChangedScene -= ChangeCurrentScene;
+            GoToPortal.OnChangedScene -= ChangeCurrentScene;
+            VignetteLerp.OnChengedScene -= ChangeCurrentScene;
+        }
+    }
+}
