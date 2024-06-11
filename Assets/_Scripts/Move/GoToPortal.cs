63912f397142680bd9407b40ab4f5808e1b32ec2
TrapTap277 <145903439+TrapTap277@users.noreply.github.com>
2024-06-11 12:34:46 +0300

    Added general scripts for Start scene

---
+++ Assets/_Scripts/Move/GoToPortal.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+
+namespace _Scripts.Move
+{
+    public class GoToPortal : MonoBehaviour
+    {
+        public static event Action<int> OnChangedScene;
+        public static event Action<int> OnOpenedDoor;
+        public static event Action OnOpenedPortal;
+
+        [SerializeField] private Transform[] _path;
+
+        private const float TIME_TO_GO = 2f;
+        private int _moves;
+        private int _doorIndex;
+
+        void Start()
+        {
+            _doorIndex = 999999;
+
+            StartCoroutine(Go());
+        }
+
+        public IEnumerator Go()
+        {
+            Sequence move = DOTween.Sequence();
+
+            Vector3[] pathPositions = new Vector3[_path.Length];
+            for (int i = 0; i < _path.Length; i++)
+            {
+                pathPositions[i] = _path[i].position;
+            }
+
+            foreach (var point in _path)
+            {
+                yield return new WaitForSeconds(1f);
+                _moves++;
+
+                _doorIndex = ChangeDoorIndex();
+
+                move.Append(gameObject.transform.DOMove(point.position, TIME_TO_GO).SetEase(Ease.Linear));
+                yield return new WaitForSeconds(2f);
+                move.Append(gameObject.transform.DORotate(Vector3.zero, 1f).SetEase(Ease.Flash));
+
+                if(_doorIndex == 0 || _doorIndex == 1)
+                {
+                    OnOpenedDoor?.Invoke(_doorIndex);
+
+                    yield return new WaitForSeconds(4.5f);
+                }
+
+                if(_moves == 3)
+                {
+                    OnOpenedPortal?.Invoke();
+                    yield return new WaitForSeconds(2);
+                }
+
+                if(_moves == 5)
+                {
+                    OnChangedScene?.Invoke(2);
+                }
+            }
+        }
+
+        private int ChangeDoorIndex()
+        {
+            if (_moves == 1)
+                return 0;
+
+            if (_moves == 2)
+                return 1;
+
+            return -1;
+        }
+    }
+}
---
+++ Assets/_Scripts/Staff/OpenDoor.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using _Scripts.Move;
+using UnityEngine;
+
+namespace _Scripts.Staff
+{
+    public class OpenDoor : MonoBehaviour
+    {
+        //public List<CurrentAbility> _allAbilities; // Todo Instantiate random ability
+
+        [SerializeField] private Animator _staffAnimation;
+        [SerializeField] private GameObject _portal;
+        [SerializeField] private List<GameObject> _doors;
+
+        public void DestroyDoor(int index)
+        {
+            StartCoroutine(DestroyDoorWithTime(index));
+        }
+
+        public void OpenPortal()
+        {
+            StartCoroutine(OpenPortalWithTime());
+        }
+
+        public IEnumerator OpenPortalWithTime()
+        {
+            if (_staffAnimation != null)
+            {
+                _staffAnimation.enabled = true;
+
+                yield return new WaitForSeconds(2f);
+
+                _staffAnimation.enabled = false;
+            }
+
+            if (_portal != null) _portal.SetActive(true);
+        }
+
+        public IEnumerator DestroyDoorWithTime(int index)
+        {
+            if (_staffAnimation != null)
+            {
+                _staffAnimation.enabled = true;
+
+                yield return new WaitForSeconds(2f);
+
+                //StartCoroutine(UseMagic());
+
+                yield return new WaitForSeconds(2f);
+                _staffAnimation.enabled = false;
+                yield return new WaitForSeconds(2f);
+            }
+
+            if (_doors.Count > index && _doors[index] != null) Destroy(_doors[index]);
+        }
+
+        // private IEnumerator UseMagic()
+        // {
+        //     if (_allAbilities.Count > 0)
+        //     {
+        //         var getRandomAbility = Random.Range(0, _allAbilities.Count);
+        //         GameObject newAbility = Instantiate(_allAbilities[getRandomAbility].Effect, 
+        //             gameObject.transform.position, Quaternion.identity); // Todo Instantiate random ability
+        //
+        //         yield return new WaitForSeconds(4f);
+        //
+        //         var explosion = GameObject.FindGameObjectWithTag("Destroy");
+        //
+        //         if (newAbility != null) Destroy(newAbility);
+        //
+        //         if (explosion != null) Destroy(explosion);
+        //     }
+        // }
+
+        private void OnEnable()
+        {
+            GoToPortal.OnOpenedDoor += DestroyDoor;
+            GoToPortal.OnOpenedPortal += OpenPortal;
+        }
+
+        private void OnDisable()
+        {
+            GoToPortal.OnOpenedDoor -= DestroyDoor;
+            GoToPortal.OnOpenedPortal -= OpenPortal;
+        }
+    }
+}
