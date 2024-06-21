30e3208fb4ba504eabdf44c18971254a52dbf62b
TrapTap277 <145903439+TrapTap277@users.noreply.github.com>
2024-06-21 19:19:57 +0300

    Changed Effects(Added collision)

---
+++ Assets/_Scripts/Particles/ParticleDealDamage.cs
@@ -0,0 +1,13 @@
+﻿using _Scripts.Health;
+using UnityEngine;
+
+namespace _Scripts.Particles
+{
+    public class ParticleDealDamage : MonoBehaviour
+    {
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.TryGetComponent(out HealthBase healthBase)) healthBase.TakeDamage(20);
+        }
+    }
+}
