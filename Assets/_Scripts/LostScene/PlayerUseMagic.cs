be22514b062ac59aca3df5ced877827ce1b880be
TrapTap277 <145903439+TrapTap277@users.noreply.github.com>
2024-06-21 17:18:10 +0300

    Added Use Magic Logic

---
+++ Assets/_Scripts/LostScene/PlayerUseMagic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using _Scripts.Shooting;
+using UnityEngine;
+
+namespace _Scripts.LostScene
+{
+    public class PlayerUseMagic : BaseUseMagic
+    {
+        [SerializeField] private List<GameObject> _abilities = new List<GameObject>();
+        [SerializeField] private GameObject _openPortalAbility;
+        [SerializeField] private Transform _staffPositions;
+        [SerializeField] private Transform _scythePositions;
+
+        private protected override void Init()
+        {
+            Abilities = _abilities;
+            CustomAbility = _openPortalAbility;
+
+            AbilitySpawnPositions.Clear();
+
+            AbilitySpawnPositions[ShootIn.Enemy] = _staffPositions;
+            AbilitySpawnPositions[ShootIn.Player] = _scythePositions;
+        }
+    }
+}
