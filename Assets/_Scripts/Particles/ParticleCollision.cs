665990d5327edf13194d46605e7d408ab6ae5097
TrapTap277 <145903439+TrapTap277@users.noreply.github.com>
2024-06-23 17:51:35 +0300

    Added dealing damage through particle collision

--- Assets/_Scripts/Particles/ParticleCollision.cs
+++ Assets/_Scripts/Particles/ParticleCollision.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using _Scripts.Health;
 using _Scripts.LostScene;
 using UnityEngine;
@@ -7,11 +8,18 @@ namespace _Scripts.Particles
 {
     public class ParticleCollision : MonoBehaviour
     {
+        public static event Action OnResetedProperties;
+
         [SerializeField] private int _timeToDissolve;
 
         private async void OnCollisionEnter(Collision other)
         {
-            if (other.gameObject.TryGetComponent(out HealthBase healthBase)) healthBase.TakeDamage(20);
+            if (other.gameObject.TryGetComponent(out HealthBase healthBase))
+            {
+                healthBase.TakeDamage(20);
+                OnResetedProperties?.Invoke();
+            }
+
             if (!other.gameObject.TryGetComponent(out DissolveDoor dissolveDoor)) return;
             await Task.Delay(_timeToDissolve);
             dissolveDoor.Dissolve();
